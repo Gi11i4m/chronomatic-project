b4b14e63afb7203583c1cebb58e429a95937b3c3
Yolan Coeman <yolan@coeman.net>
2013-03-22 16:14:35 +0100

    merge lokale opslag poging 2

---
+++ ChronomaticServer/src/chronomatic/database/DatabaseContainer.java.BASE.10624.java
@@ -0,0 +1,21 @@
+package chronomatic.database;
+
+import java.sql.*;
+
+/**
+ * Deze klasse haalt de configuratie op voor de huidige server
+ * Tijdelijk worden de waardes in de code zelf aangepast, later komt dit uit een .xml bestand of dergelijke.
+ * @author Daim
+ *
+ */
+public class DatabaseContainer {
+	private static String host = "127.0.0.1";
+	private static String user = "root";
+	private static String password = "";
+	private static String db_name = "chrono_matic";
+	
+	public static Connection getConnection() {
+		return Database.getConnection(host,user,password,db_name);
+	}
+	
+}
